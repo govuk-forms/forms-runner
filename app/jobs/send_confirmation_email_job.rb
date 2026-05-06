508f1cc362c35fbaa8e426c2c83a28f8eabf2f29
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2026-05-06 12:38:43 +0100

    Update send_confirmation_email_job.rb to explicitly send English and Welsh
    Unfortunately have to discard relying on the I18n locale as we are using two languages at this point, whereas I18n will use whatever singular locale is set.

--- app/jobs/send_confirmation_email_job.rb
+++ app/jobs/send_confirmation_email_job.rb
@@ -5,19 +5,21 @@ class SendConfirmationEmailJob < ApplicationJob
   def perform(submission:, notify_response_id:, confirmation_email_address:)
     set_submission_logging_attributes(submission:)
 
-    I18n.with_locale(submission.submission_locale || I18n.default_locale) do
-      form = submission.form
-      mail = FormSubmissionConfirmationMailer.send_confirmation_email(
-        what_happens_next_markdown: form.what_happens_next_markdown,
-        support_contact_details: form.support_details,
-        notify_response_id:,
-        confirmation_email_address:,
-        mailer_options: mailer_options_for(submission:, form:),
-      )
+    form = submission.form
+    welsh_form = fetch_welsh_form(submission:, form:)
+    mail = FormSubmissionConfirmationMailer.send_confirmation_email(
+      what_happens_next_markdown: form.what_happens_next_markdown,
+      what_happens_next_markdown_cy: welsh_form&.what_happens_next_markdown,
+      support_contact_details: form.support_details,
+      support_contact_details_cy: welsh_form&.support_details,
+      notify_response_id:,
+      confirmation_email_address:,
+      mailer_options: mailer_options_for(submission:, form:),
+      submission_locale: submission.submission_locale,
+    )
 
-      mail.deliver_now
-      CurrentJobLoggingAttributes.confirmation_email_id = mail.govuk_notify_response.id
-    end
+    mail.deliver_now
+    CurrentJobLoggingAttributes.confirmation_email_id = mail.govuk_notify_response.id
   rescue StandardError
     CloudWatchService.record_job_failure_metric(self.class.name)
     raise
@@ -33,5 +35,16 @@ private
       submission_reference: submission.reference,
       payment_url: submission.payment_url,
     )
+  end
+
+  def fetch_welsh_form(submission:, form:)
+    return nil unless submission.submission_locale.to_sym == :cy
+
+    form_document = Api::V2::FormDocumentRepository.find_with_mode(
+      form_id: form.id,
+      mode: submission.mode_object,
+      language: :cy,
+    )
+    Form.new(form_document) if form_document
   end
 end
