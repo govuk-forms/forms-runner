03a78b81235771e0651db93dc7dfbc7eac7305c6
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-05-11 12:00:54 +0100

    Add job to schedule job to send bounce notifications
    We want the job that sends bounce notifications to run on a schedule
    every day, and we want it to be retryable if it fails. Add a job that
    will run on a schdedule to enqueue the SendBounceNotificationsJob to
    send bounce notifications for the previous day so that retrying the
    job will send bounces for that day.

---
+++ app/jobs/schedule_bounce_notifications_job.rb
@@ -0,0 +1,11 @@
+class ScheduleBounceNotificationsJob < ApplicationJob
+  queue_as :bounce_notifications
+
+  def perform
+    CloudWatchService.record_job_started_metric(self.class.name)
+    CurrentJobLoggingAttributes.job_class = self.class.name
+    CurrentJobLoggingAttributes.job_id = job_id
+
+    SendBounceNotificationsJob.perform_later(bounced_on_date: Time.zone.yesterday.to_date)
+  end
+end
