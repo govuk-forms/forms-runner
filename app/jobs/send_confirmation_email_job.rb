132376ed3d70b3d218036dc133fc1b144e933fd2
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-05-26 16:53:44 +0100

    Send confirmation email via SES if including answers
    Accept an include_copy_of_answers parameter to
    SendConfirmationEmailJob. If this is set to true, use SES to send the
    confirmation email, including the answer in the email.
    
    In future we'll use SES to send all confirmation emails, but we want
    to make sure this is working correctly just for emails that include a
    copy of the answers first.

--- app/jobs/send_confirmation_email_job.rb
+++ app/jobs/send_confirmation_email_job.rb
@@ -1,17 +1,21 @@
 class SendConfirmationEmailJob < ApplicationJob
   queue_as :confirmation_emails
 
-  def perform(submission:, notify_response_id:, confirmation_email_address:)
+  def perform(submission:, notify_response_id:, confirmation_email_address:, include_copy_of_answers: false)
     set_submission_logging_attributes(submission:)
 
-    mail = FormSubmissionConfirmationMailer.send_confirmation_email(
-      submission:,
-      notify_response_id:,
-      confirmation_email_address:,
-    )
+    mail = if include_copy_of_answers
+             AwsSesSubmissionConfirmationMailer.submission_confirmation_email(
+               submission:, confirmation_email_address:, include_copy_of_answers:,
+             )
+           else
+             FormSubmissionConfirmationMailer.send_confirmation_email(
+               submission:, notify_response_id:, confirmation_email_address:,
+             )
+           end
 
     mail.deliver_now
-    CurrentJobLoggingAttributes.confirmation_email_id = mail.govuk_notify_response.id
+    CurrentJobLoggingAttributes.confirmation_email_id = mail.govuk_notify_response&.id.presence || mail.message_id
   rescue StandardError
     CloudWatchService.record_job_failure_metric(self.class.name)
     raise
