394b32f0fa09ce075f7c0e6abd987ba7e1848e16
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2026-06-16 14:43:34 +0100

    Use SES to send all confirmation emails
    we currently use SES to send confirmation emails that include a copy of the user's answers, and notify to send the confirmation emails that don't.
    This commit changes this so that all confirmation emails are sent with SES. it also updates the tests to support this.

--- app/jobs/send_confirmation_email_job.rb
+++ app/jobs/send_confirmation_email_job.rb
@@ -1,24 +1,18 @@
 class SendConfirmationEmailJob < ApplicationJob
   queue_as :confirmation_emails
 
-  def perform(submission:, notify_response_id:, confirmation_email_address:, include_copy_of_answers: false)
+  def perform(submission:, confirmation_email_address:, include_copy_of_answers: false)
     set_submission_logging_attributes(submission:)
 
     # The job will use the locale at the time it was created. Force it to be "en" as we send multilingual emails for
     # forms submitted in Welsh.
     I18n.with_locale("en") do
-      mail = if include_copy_of_answers
-               AwsSesSubmissionConfirmationMailer.submission_confirmation_email(
-                 submission:, confirmation_email_address:, include_copy_of_answers:,
-               )
-             else
-               FormSubmissionConfirmationMailer.send_confirmation_email(
-                 submission:, notify_response_id:, confirmation_email_address:,
-               )
-             end
+      mail = AwsSesSubmissionConfirmationMailer.submission_confirmation_email(
+        submission:, confirmation_email_address:, include_copy_of_answers:,
+      )
 
       mail.deliver_now
-      CurrentJobLoggingAttributes.confirmation_email_id = mail.govuk_notify_response&.id.presence || mail.message_id
+      CurrentJobLoggingAttributes.confirmation_email_id = mail.message_id
     end
   rescue StandardError
     CloudWatchService.record_job_failure_metric(self.class.name)
