20f4c2de86c5493349b750e3b8de01b385b087ee
alice-carr <52633448+alice-carr@users.noreply.github.com>
2026-08-11 16:56:25 +0100

    Revert "Use SES to send all confirmation emails"

--- app/jobs/send_confirmation_email_job.rb
+++ app/jobs/send_confirmation_email_job.rb
@@ -1,21 +1,24 @@
 class SendConfirmationEmailJob < ApplicationJob
   queue_as :confirmation_emails
 
-  # TODO: remove notify_response_id once deployed.
-  # we are passing this is so as not to break any job already serialised in solid_queue at deploy time.
-  # rubocop:disable Lint/UnusedMethodArgument
-  def perform(submission:, confirmation_email_address:, notify_response_id: nil, include_copy_of_answers: false)
+  def perform(submission:, notify_response_id:, confirmation_email_address:, include_copy_of_answers: false)
     set_submission_logging_attributes(submission:)
-    # rubocop:enable Lint/UnusedMethodArgument
+
     # The job will use the locale at the time it was created. Force it to be "en" as we send multilingual emails for
     # forms submitted in Welsh.
     I18n.with_locale("en") do
-      mail = AwsSesSubmissionConfirmationMailer.submission_confirmation_email(
-        submission:, confirmation_email_address:, include_copy_of_answers:,
-      )
+      mail = if include_copy_of_answers
+               AwsSesSubmissionConfirmationMailer.submission_confirmation_email(
+                 submission:, confirmation_email_address:, include_copy_of_answers:,
+               )
+             else
+               FormSubmissionConfirmationMailer.send_confirmation_email(
+                 submission:, notify_response_id:, confirmation_email_address:,
+               )
+             end
 
       mail.deliver_now
-      CurrentJobLoggingAttributes.confirmation_email_id = mail.message_id
+      CurrentJobLoggingAttributes.confirmation_email_id = mail.govuk_notify_response&.id.presence || mail.message_id
     end
   rescue StandardError
     CloudWatchService.record_job_failure_metric(self.class.name)
