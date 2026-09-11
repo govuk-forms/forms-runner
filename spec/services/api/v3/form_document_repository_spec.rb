618ddc23e8457f2a006af291525d3829839fe7a0
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2026-09-11 16:30:19 +0100

    Mock FormDocumentResource in repository spec
    The tests in this file were mocking the V3 API HTTP requests, but we're
    already testing those in the FormDocumentResource, so the tests here
    were a bit repetitive and made some assumptions about how the
    FormDocumentResource was implemented.
    
    This commit rewrites the test to mock the FormDocumentResource, so we
    don't have to make assumptions about its implementation and can just
    test that the repository is calling it correctly. I think this makes the
    tests a bit more readable and resilient to changes in the Resource code
    and the API.

--- spec/services/api/v3/form_document_repository_spec.rb
+++ spec/services/api/v3/form_document_repository_spec.rb
@@ -10,16 +10,12 @@ RSpec.describe Api::V3::FormDocumentRepository do
   describe ".find_by_tag" do
     let(:form_id) { "1" }
     let(:tag) { :live }
-    let(:response_data) { api_v3_response_data.to_json }
-    let(:welsh_response_data) { api_v3_welsh_response_data.to_json }
-    let(:status) { 200 }
+    let(:response_data) { api_v3_response_data }
+    let(:welsh_response_data) { api_v3_welsh_response_data }
     let(:language) { nil }
 
     before do
-      ActiveResource::HttpMock.respond_to do |mock|
-        mock.get "/api/v3/forms/#{form_id}/versions/#{tag}", req_headers, response_data, status
-        mock.get "/api/v3/forms/#{form_id}/versions/#{tag}?language=cy", req_headers, welsh_response_data, status
-      end
+      allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with("1", :live).and_return(response_data)
     end
 
     it "returns form document" do
@@ -32,19 +28,14 @@ RSpec.describe Api::V3::FormDocumentRepository do
       let(:tag) { :archived }
 
       context "when form has been archived" do
+        before do
+          allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with("1", :archived).and_return(response_data)
+        end
+
         it "returns an archived form document" do
           form = described_class.find_by_tag(tag: :archived, form_id: form_id)
 
           expect(form).to have_attributes(form_id: form_id, name: "All question types form")
-        end
-      end
-
-      context "when a form has not been archived" do
-        let(:response_data) { nil }
-        let(:status) { 404 }
-
-        it "returns nil" do
-          expect(described_class.find_by_tag(tag: :archived, form_id: form_id)).to be_nil
         end
       end
     end
@@ -65,34 +56,38 @@ RSpec.describe Api::V3::FormDocumentRepository do
       end
     end
 
-    context "when the form does not exist" do
-      let(:form_id) { "99" }
-      let(:response_data) { nil }
-      let(:status) { 404 }
+    context "when the form document is not found" do
+      before do
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with("1", :live).and_raise(ActiveResource::ResourceNotFound.new(nil))
+      end
 
       it "returns nil" do
         expect(described_class.find_by_tag(tag:, form_id:)).to be_nil
       end
     end
 
-    context "when given language makes correct request" do
-      let(:request_with_no_language_param) { ActiveResource::Request.new(:get, "/api/v3/forms/#{form_id}/versions/#{tag}", req_headers) }
-      let(:request_with_language_param_cy) { ActiveResource::Request.new(:get, "/api/v3/forms/#{form_id}/versions/#{tag}?language=cy", req_headers) }
-
+    context "when called with a language param" do
       before do
-        mock_response = ActiveResource::Response.new(response_data)
-        ActiveResource::HttpMock.respond_to(request_with_no_language_param => mock_response,
-                                            request_with_language_param_cy => mock_response)
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(form_id, tag).and_return(response_data)
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(form_id, tag, language: :cy).and_return(welsh_response_data)
       end
 
-      it "given :en makes request without the language param" do
-        described_class.find_by_tag(tag:, form_id:, language: :en)
-        expect(ActiveResource::HttpMock.requests).to include request_with_no_language_param
+      context "and the language is English" do
+        let(:language) { :en }
+
+        it "does not call the FormDocumentResource with the language param" do
+          described_class.find_by_tag(tag:, form_id:, language:)
+          expect(Api::V3::FormDocumentResource).to have_received(:find_by_tag).with(form_id, tag)
+        end
       end
 
-      it "given :cy makes request with the language param" do
-        described_class.find_by_tag(tag:, form_id:, language: :cy)
-        expect(ActiveResource::HttpMock.requests).to include request_with_language_param_cy
+      context "and the language is Welsh" do
+        let(:language) { :cy }
+
+        it "calls the FormDocumentResource with the language param" do
+          described_class.find_by_tag(tag:, form_id:, language:)
+          expect(Api::V3::FormDocumentResource).to have_received(:find_by_tag).with(form_id, tag, { language: })
+        end
       end
     end
   end
@@ -100,16 +95,12 @@ RSpec.describe Api::V3::FormDocumentRepository do
   describe ".find_by_version" do
     let(:form_id) { "1" }
     let(:version) { 3 }
-    let(:response_data) { api_v3_response_data.to_json }
-    let(:welsh_response_data) { api_v3_welsh_response_data.to_json }
-    let(:status) { 200 }
+    let(:response_data) { api_v3_response_data }
+    let(:welsh_response_data) { api_v3_welsh_response_data }
     let(:language) { nil }
 
     before do
-      ActiveResource::HttpMock.respond_to do |mock|
-        mock.get "/api/v3/forms/#{form_id}/versions/#{version}", req_headers, response_data, status
-        mock.get "/api/v3/forms/#{form_id}/versions/#{version}?language=cy", req_headers, welsh_response_data, status
-      end
+      allow(Api::V3::FormDocumentResource).to receive(:find_by_version).with("1", 3).and_return(response_data)
     end
 
     it "returns form document" do
@@ -142,59 +133,45 @@ RSpec.describe Api::V3::FormDocumentRepository do
       end
     end
 
-    context "when the form does not exist" do
-      let(:form_id) { "99" }
-      let(:response_data) { nil }
-      let(:status) { 404 }
-
-      it "returns nil" do
-        expect(described_class.find_by_version(version:, form_id:)).to be_nil
-      end
-    end
-
-    context "when the version does not exist" do
-      let(:version) { 5 }
-      let(:response_data) { nil }
-      let(:status) { 404 }
-
-      it "returns nil" do
-        expect(described_class.find_by_version(version:, form_id:)).to be_nil
-      end
-    end
-
-    context "when given language makes correct request" do
-      let(:request_with_no_language_param) { ActiveResource::Request.new(:get, "/api/v3/forms/#{form_id}/versions/#{version}", req_headers) }
-      let(:request_with_language_param_cy) { ActiveResource::Request.new(:get, "/api/v3/forms/#{form_id}/versions/#{version}?language=cy", req_headers) }
-
+    context "when the form document is not found" do
       before do
-        mock_response = ActiveResource::Response.new(response_data)
-        ActiveResource::HttpMock.respond_to(request_with_no_language_param => mock_response,
-                                            request_with_language_param_cy => mock_response)
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_version).with(form_id, version).and_raise(ActiveResource::ResourceNotFound.new(nil))
       end
 
-      it "given :en makes request without the language param" do
-        described_class.find_by_version(version:, form_id:, language: :en)
-        expect(ActiveResource::HttpMock.requests).to include request_with_no_language_param
+      it "returns nil" do
+        expect(described_class.find_by_version(version:, form_id:)).to be_nil
+      end
+    end
+
+    context "when called with a language param" do
+      before do
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_version).with(form_id, version).and_return(response_data)
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_version).with(form_id, version, language: :cy).and_return(welsh_response_data)
       end
 
-      it "given :cy makes request with the language param" do
-        described_class.find_by_version(version:, form_id:, language: :cy)
-        expect(ActiveResource::HttpMock.requests).to include request_with_language_param_cy
+      context "and the language is English" do
+        let(:language) { :en }
+
+        it "does not call the FormDocumentResource with the language param" do
+          described_class.find_by_version(version:, form_id:, language:)
+          expect(Api::V3::FormDocumentResource).to have_received(:find_by_version).with(form_id, version)
+        end
+      end
+
+      context "and the language is Welsh" do
+        let(:language) { :cy }
+
+        it "calls the FormDocumentResource with the language param" do
+          described_class.find_by_version(version:, form_id:, language:)
+          expect(Api::V3::FormDocumentResource).to have_received(:find_by_version).with(form_id, version, language:)
+        end
       end
     end
   end
 
   describe ".find_with_mode" do
     before do
-      ActiveResource::HttpMock.respond_to do |mock|
-        mock.get "/api/v3/forms/1/versions/draft", req_headers, api_v3_response_data.merge(name: "Draft form").to_json, 200
-        mock.get "/api/v3/forms/1/versions/live", req_headers, api_v3_response_data.merge(name: "Live form").to_json, 200
-        mock.get "/api/v3/forms/1/versions/archived", req_headers, api_v3_response_data.merge(name: "Archived form").to_json, 200
-        mock.get "/api/v3/forms/2/versions/live", req_headers, nil, 404
-        mock.get "/api/v3/forms/2/versions/archived", req_headers, api_v3_response_data.to_json, 200
-        mock.get "/api/v3/forms/Alpha123/versions/draft", req_headers, api_v3_response_data.merge("form_id": "Alpha123").to_json, 200
-        mock.get "/api/v3/forms/99/versions/draft", req_headers, nil, 404
-      end
+      allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(1, :draft).and_return(api_v3_response_data)
     end
 
     it "finds a form document given a form id and document tag" do
@@ -207,43 +184,69 @@ RSpec.describe Api::V3::FormDocumentRepository do
       expect(form_snapshot.steps).to all be_a Api::V2::StepResource
     end
 
-    it "returns nil if the form does not exist" do
-      expect(described_class.find_with_mode(form_id: "99", mode: Mode.new("preview-draft"))).to be_nil
+    context "when the form document is not found" do
+      before do
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(1, :draft).and_raise(ActiveResource::ResourceNotFound.new(nil))
+      end
+
+      it "returns nil" do
+        expect(described_class.find_with_mode(form_id: 1, mode: Mode.new("preview-draft"))).to be_nil
+      end
     end
 
     context "when mode is live" do
-      it "returns a live form" do
-        form = described_class.find_with_mode(form_id: "1", mode: Mode.new("live"))
+      before do
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(1, :live).and_return(api_v3_response_data)
+      end
 
-        expect(form).to have_attributes(form_id: "1", name: "Live form")
+      it "gets a live form document from FormDocumentResource" do
+        described_class.find_with_mode(form_id: 1, mode: Mode.new("live"))
+
+        expect(Api::V3::FormDocumentResource).to have_received(:find_by_tag).with(1, :live)
       end
     end
 
     context "when mode is draft" do
-      it "returns a draft form" do
-        form = described_class.find_with_mode(form_id: "1", mode: Mode.new("preview-draft"))
+      before do
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(1, :draft).and_return(api_v3_response_data)
+      end
 
-        expect(form).to have_attributes(form_id: "1", name: "Draft form")
+      it "gets a draft form document from FormDocumentResource" do
+        described_class.find_with_mode(form_id: 1, mode: Mode.new("preview-draft"))
+
+        expect(Api::V3::FormDocumentResource).to have_received(:find_by_tag).with(1, :draft)
       end
     end
 
     context "when mode is archived" do
-      it "returns an archived form" do
-        form = described_class.find_with_mode(form_id: "1", mode: Mode.new("preview-archived"))
+      before do
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(1, :archived).and_return(api_v3_response_data)
+      end
 
-        expect(form).to have_attributes(form_id: "1", name: "Archived form")
+      it "gets an archived form document from FormDocumentResource" do
+        described_class.find_with_mode(form_id: 1, mode: Mode.new("preview-archived"))
+
+        expect(Api::V3::FormDocumentResource).to have_received(:find_by_tag).with(1, :archived)
       end
     end
 
     context "when mode is preview live" do
-      it "returns a live form" do
-        form = described_class.find_with_mode(form_id: "1", mode: Mode.new("preview-live"))
+      before do
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(1, :live).and_return(api_v3_response_data)
+      end
 
-        expect(form).to have_attributes(form_id: "1", name: "Live form")
+      it "gets a live form document from FormDocumentResource" do
+        described_class.find_with_mode(form_id: 1, mode: Mode.new("preview-live"))
+
+        expect(Api::V3::FormDocumentResource).to have_received(:find_by_tag).with(1, :live)
       end
     end
 
     context "when validating the provided form id" do
+      before do
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).and_return(api_v3_response_data)
+      end
+
       it "returns nil when the id contains non-alpha-numeric chars" do
         expect(described_class.find_with_mode(form_id: "<id>", mode: Mode.new("preview-draft"))).to be_nil
       end
@@ -255,20 +258,32 @@ RSpec.describe Api::V3::FormDocumentRepository do
       it "returns the form when the id is alphanumeric" do
         form = described_class.find_with_mode(form_id: "Alpha123", mode: Mode.new("preview-draft"))
 
-        expect(form).to have_attributes(form_id: "Alpha123", name: "All question types form")
+        expect(form).to have_attributes(name: "All question types form")
       end
     end
 
-    context "when Welsh form requested" do
+    context "when called with a language param" do
       before do
-        ActiveResource::HttpMock.respond_to do |mock|
-          mock.get "/api/v3/forms/1/versions/live?language=cy", req_headers, api_v3_welsh_response_data.to_json, 200
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(1, :draft).and_return(api_v3_response_data)
+        allow(Api::V3::FormDocumentResource).to receive(:find_by_tag).with(1, :draft, language: :cy).and_return(api_v3_welsh_response_data)
+      end
+
+      context "and the language is English" do
+        let(:language) { :en }
+
+        it "does not call the FormDocumentResource with the language param" do
+          described_class.find_with_mode(form_id: 1, mode: Mode.new("preview-draft"), language:)
+          expect(Api::V3::FormDocumentResource).to have_received(:find_by_tag).with(1, :draft)
         end
       end
 
-      it "returns Welsh form document" do
-        form = described_class.find_with_mode(form_id: "1", mode: Mode.new("live"), language: :cy)
-        expect(form).to have_attributes(form_id: "1", name: "Welsh form", language: "cy")
+      context "and the language is Welsh" do
+        let(:language) { :cy }
+
+        it "calls the FormDocumentResource with the language param" do
+          described_class.find_with_mode(form_id: 1, mode: Mode.new("preview-draft"), language:)
+          expect(Api::V3::FormDocumentResource).to have_received(:find_by_tag).with(1, :draft, { language: })
+        end
       end
     end
   end
