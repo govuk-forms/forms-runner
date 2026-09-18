fabb9c47cba92d46c1ff1e98fb7447f333fba304
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2026-09-18 15:02:04 +0100

    Move StepResource out of V2 namespace
    This resource is shared between the v2 and v3 APIs, so this namespace can be removed.

--- spec/resources/api/step_resource_spec.rb
+++ spec/resources/api/step_resource_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Api::V2::StepResource do
+RSpec.describe Api::StepResource do
   it "has a valid factory" do
     step = build :step_resource
     expect(step).to be_valid
--- spec/services/api/v2/form_document_repository_spec.rb
+++ spec/services/api/v2/form_document_repository_spec.rb
@@ -117,7 +117,7 @@ RSpec.describe Api::V2::FormDocumentRepository do
     it "returns a FormDocumentResource model" do
       form_snapshot = described_class.find_with_mode(form_id: 1, mode: Mode.new("preview-draft"))
       expect(form_snapshot).to be_a Api::V2::FormDocumentResource
-      expect(form_snapshot.steps).to all be_a Api::V2::StepResource
+      expect(form_snapshot.steps).to all be_a Api::StepResource
     end
 
     it "returns nil if the form does not exist" do
